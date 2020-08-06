a51a4420687b564c315dfbd1b310459fc0c421ce
Hyeoncheol Jung <joe.hyeoncheol.jung@gmail.com>
2020-08-06 14:34:49 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -10,22 +10,12 @@ BRANDS = []
           @brand = brand
           if !BRANDS.include?(brand)
           BRANDS << brand
-        
         end
       end
-      
-        
-        
-  
 
   def cobble
     self.condition = "new"
     puts "Your shoe is as good as new!"
   end
   
-  # def brand(brand)
-  #   @brand = brand
-  #   BRANDS << brand
-# end
-
 end
